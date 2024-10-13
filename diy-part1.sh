fb4682649351a14319069c1b564d2c42d49c02e3
Bang5385 <93089764+Bang5385@users.noreply.github.com>
2024-10-13 13:32:57 +0700

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -2,7 +2,7 @@
 svn_export() {
 	# 参数1是分支名, 参数2是子目录, 参数3是目标目录, 参数4仓库地址
 	TMP_DIR="$(mktemp -d)" || exit 1
- 	ORI_DIR="$PWD"
+	ORI_DIR="$PWD"
 	[ -d "$3" ] || mkdir -p "$3"
 	TGT_DIR="$(cd "$3"; pwd)"
 	git clone --depth 1 -b "$1" "$4" "$TMP_DIR" >/dev/null 2>&1 && \
@@ -11,14 +11,18 @@ svn_export() {
 	rm -rf "$TMP_DIR"
 }
 
+# Remove existing Makefiles related to v2ray-geodata
 find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
 
+# Clone necessary repositories
 git clone --depth 1 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
 git clone --depth 1 https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
 git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall-packages package/openwrt-passwall-packages
 git clone --depth 1 https://github.com/fw876/helloworld package/helloworld
 git clone --depth 1 https://github.com/chenmozhijin/luci-app-adguardhome package/luci-app-adguardhome
 git clone --depth 1 https://github.com/tty228/luci-app-wechatpush package/luci-app-wechatpush
+
+# Export using svn
 svn_export "main" "luci-app-passwall" "package/luci-app-passwall" "https://github.com/xiaorouji/openwrt-passwall"
 svn_export "master" "applications/luci-app-zerotier" "feeds/luci/applications/luci-app-zerotier" "https://github.com/immortalwrt/luci"
 svn_export "master" "applications/luci-app-vlmcsd" "feeds/luci/applications/luci-app-vlmcsd" "https://github.com/immortalwrt/luci"
@@ -29,35 +33,44 @@ svn_export "master" "net/ddns-scripts_dnspod" "feeds/packages/net/ddns-scripts_d
 svn_export "master" "net/vlmcsd" "feeds/packages/net/vlmcsd" "https://github.com/immortalwrt/packages"
 svn_export "master" "package/emortal" "package/emortal" "https://github.com/immortalwrt/immortalwrt"
 
-# turboacc 补丁
+# Apply turboacc patch
 curl -sSL https://raw.githubusercontent.com/chenmozhijin/turboacc/luci/add_turboacc.sh -o add_turboacc.sh && bash add_turboacc.sh
-# 安装插件
+
+# Install feeds
 ./scripts/feeds update -l
 ./scripts/feeds install -a
 
-# 编译 po2lmo (如果有po2lmo可跳过)
+# Build po2lmo if necessary (uncomment if needed)
 #pushd package/luci-app-openclash/tools/po2lmo
 #make && sudo make install
 #popd
-# 替换argon主题
+
+# Replace luci-theme-argon
 git clone https://github.com/jerrykuku/luci-theme-argon.git ./feeds/luci/themes/luci-theme-argon
-# 微信推送
+
+# WeChat push customization
 sed -i "s|qidian|bilibili|g" package/luci-app-wechatpush/root/usr/share/wechatpush/wechatpush
-# 个性化设置
+
+# Custom settings (change IP and device name)
 sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
 sed -i 's/OpenWrt/MI-R3G/' package/base-files/files/bin/config_generate
-# 汉化
+
+# Localization (convert translations)
 cd package
 curl -sfL -o ./convert_translation.sh https://github.com/kenzok8/small-package/raw/main/.github/diy/convert_translation.sh
 chmod +x ./convert_translation.sh && bash ./convert_translation.sh
-# 更新passwall规则
+
+# Update passwall rules
 curl -sfL -o ./luci-app-passwall/root/usr/share/passwall/rules/gfwlist https://raw.githubusercontent.com/Loyalsoldier/v2ray-rules-dat/release/gfw.txt
-#AdguardHome
+
+# Download and set up AdGuardHome
 cd ./package/luci-app-adguardhome/root/usr
 mkdir -p ./bin/AdGuardHome && cd ./bin/AdGuardHome
 ADG_VER=$(curl -sfL https://api.github.com/repos/AdguardTeam/AdGuardHome/releases 2>/dev/null | grep 'tag_name' | egrep -o "v[0-9].+[0-9.]" | awk 'NR==1')
 curl -sfL -o /tmp/AdGuardHome_linux.tar.gz https://github.com/AdguardTeam/AdGuardHome/releases/download/${ADG_VER}/AdGuardHome_linux_mipsle_softfloat.tar.gz
 tar -zxf /tmp/*.tar.gz -C /tmp/ && chmod +x /tmp/AdGuardHome/AdGuardHome
+
+# Compress AdGuardHome binary using UPX
 upx_latest_ver="$(curl -sfL https://api.github.com/repos/upx/upx/releases/latest 2>/dev/null | egrep 'tag_name' | egrep '[0-9.]+' -o 2>/dev/null)"
 curl -sfL -o /tmp/upx-${upx_latest_ver}-amd64_linux.tar.xz "https://github.com/upx/upx/releases/download/v${upx_latest_ver}/upx-${upx_latest_ver}-amd64_linux.tar.xz"
 xz -d -c /tmp/upx-${upx_latest_ver}-amd64_linux.tar.xz | tar -x -C "/tmp"
